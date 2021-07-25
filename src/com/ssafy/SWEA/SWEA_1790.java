13acf8cc85c54331157d3cfa4ff2f28a691eebe0
uoayop <uoayop@kakao.com>
2021-07-25 23:05:20 +0900

    210725

---
+++ src/com/ssafy/SWEA/SWEA_1790.java
@@ -0,0 +1,53 @@
+// 쉬운 거스름돈
+
+package com.ssafy.SWEA;
+
+import java.util.Scanner;
+
+public class SWEA_1790 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int t=1; t<=T; t++) {
+			int[] coins = {0, 0, 0, 0, 0, 0, 0, 0};
+			// 50000, 10000, 5000, 1000, 500, 100, 50, 10
+
+			int N = sc.nextInt();
+			
+			while (N>=10) {
+				if (N>=50000) {
+					coins[0] += N/50000;
+					N %= 50000;
+				} else if (N>=10000) {
+					coins[1] += N/10000;
+					N %= 10000;
+				} else if (N>=5000) {
+					coins[2] += N/5000;
+					N %= 5000;
+				} else if (N>=1000) {
+					coins[3] += N/1000;
+					N %= 1000;
+				} else if (N>=500) {
+					coins[4] += N/500;
+					N %= 500;
+				} else if (N>=100) {
+					coins[5] += N/100;
+					N %= 100;
+				} else if (N>=50) {
+					coins[6] += N/50;
+					N %= 50;
+				} else if (N>=10) {
+					coins[7] += N/10;
+					N %= 10;
+				} 
+			}
+			
+			System.out.println("#"+t);
+			for (int coin:coins) {
+				System.out.print(coin+" ");
+			}
+			System.out.println();
+		}
+	}
+}
--- src/com/ssafy/SWEA/SWEA_1976.java
+++ src/com/ssafy/SWEA/SWEA_1976.java
@@ -8,25 +8,26 @@ public class SWEA_1976 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-//		int T = sc.nextInt();
-		
-		int h1 = sc.nextInt();
-		int m1 = sc.nextInt();
-		int h2 = sc.nextInt();
-		int m2 = sc.nextInt();
-		
-		int h = h1 + h2;
-		int m = m1 + m2;
-		
-		if (m >= 60) {
-			h += m / 60;
-			m %= 60;
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++) {
+			int h1 = sc.nextInt();
+			int m1 = sc.nextInt();
+			int h2 = sc.nextInt();
+			int m2 = sc.nextInt();
+			
+			int h = h1 + h2;
+			int m = m1 + m2;
+			
+			if (m >= 60) {
+				h += m / 60;
+				m %= 60;
+			}
+			if (h > 12) {
+				h -= 12;
+			}
+			
+			System.out.printf("#%d %d %d\n",t, h, m);
 		}
-		if (h > 12) {
-			h -= 12;
-		}
-		
-		System.out.printf("%d %d\n", h, m);
 		
 	}
 }
