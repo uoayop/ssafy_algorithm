ae368a8032de52bd8a57a1a2bb08406be0457685
uoayop <uoayop@kakao.com>
2021-08-02 22:03:49 +0900

    210801

---
+++ src/com/ssafy/SWEA/SWEA_1288.java
@@ -0,0 +1,19 @@
+// 새로운 불면증 치료법 
+
+package com.ssafy.SWEA;
+
+import java.util.Scanner;
+
+public class SWEA_1288 {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++) {
+			int n = sc.nextInt();
+			
+			for (int j=0; j<10; j++) {
+			}
+		}
+	}
+}
